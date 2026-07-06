699faefeba38e890d83e8f43240cdbed5da63624
Aria Amini <amini5454@gmail.com>
2026-07-06 01:17:30 -0400

    refactor: delete co-located tests + screenshots

--- src/routes/(app)/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@tests/__mocks__/browser'
-
-import { test } from '@tests/__fixtures__/browser'
-
-import { expectRouteScreenshot } from '@tests/utils/file-routes'
-
-test('home page matches desktop screenshot', async () => {
-	await expectRouteScreenshot({ path: '/', name: 'home' })
-})
