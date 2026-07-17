e6d4ca881b89255ede3755635245674e1c48b000
Aria Amini <57912635+aria-amini@users.noreply.github.com>
2026-07-17 05:18:03 -0400

    fix migration script (#36)

--- src/db/migrations/0007_daffy_spyke.sql
+++ src/db/migrations/0007_daffy_spyke.sql
@@ -1,5 +1,6 @@
 ALTER TABLE "agent_profiles" ALTER COLUMN "not_fit_for" SET DATA TYPE text[] USING "not_fit_for"::text[];--> statement-breakpoint
 ALTER TABLE "agent_profiles" ALTER COLUMN "not_fit_for" SET DEFAULT '{}';--> statement-breakpoint
+UPDATE "agent_profiles" SET "not_fit_for" = '{}' WHERE "not_fit_for" IS NULL;--> statement-breakpoint
 ALTER TABLE "agent_profiles" ALTER COLUMN "not_fit_for" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "agent_profiles" ADD COLUMN "city_center_latitude" real DEFAULT NULL;--> statement-breakpoint
 ALTER TABLE "agent_profiles" ADD COLUMN "city_center_longitude" real DEFAULT NULL;--> statement-breakpoint
