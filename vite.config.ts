21ad5b8cca7b30df33baeb89ee8da1ad89eceffa
Aria Amini <amini5454@gmail.com>
2026-07-19 21:29:14 -0400

    fix mocks

--- vite.config.ts
+++ vite.config.ts
@@ -158,7 +158,7 @@ export default defineConfig({
 				test: {
 					name: 'server',
 					include: ['src/**/*.{server,db}.test.ts'],
-					testTimeout: 5_000,
+					testTimeout: 10_000,
 				},
 			},
 			{
@@ -168,6 +168,7 @@ export default defineConfig({
 					include: ['src/**/*.test.tsx', 'tests/pages/**/*.test.tsx'],
 					setupFiles: ['./tests/support/mocks/styles.ts'],
 					testTimeout: 15_000,
+					sequence: { groupOrder: 1 },
 					browser: {
 						instances: [{ browser: 'chromium' }],
 						provider: playwright(),
