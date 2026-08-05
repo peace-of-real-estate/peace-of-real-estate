b9d43a1eaf7f9a56f41233fbb86712262f414544
Aria Amini <57912635+aria-amini@users.noreply.github.com>
2026-08-05 07:16:33 -0400

    cleanup (#131)

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite-plus/client" />
-
-interface ViteTypeOptions {
-	strictImportMetaEnv: unknown
-}
-
-interface ImportMetaEnv {
-	readonly VITE_PUBLIC_POSTHOG_KEY?: string
-}
